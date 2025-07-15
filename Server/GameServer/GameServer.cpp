a74edf0004d47cf17e6a900b5dda9483c23088ab
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2025-07-15 22:39:06 +0900

    future

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,51 +5,23 @@
 #include <atomic>
 #include <mutex>
 
-mutex m;
-queue<int32> q;
-
-condition_variable cv;
-void Producer()
+#include<future>
+int64 Calculate()
 {
-	while (true)
-	{
-		{
-			unique_lock<mutex> lock(m);
-
-			q.push(100);
-		}
-		cv.notify_one();
-		//락을 잡고
-		//공유 변수 수정
-		//락을 풀고
-		//다른 쓰레드에게 통지
-	}
-	
-};
-void Consumer()
-{
-	while (true)
-	{
-		unique_lock<mutex> lock(m);
-		cv.wait(lock, []() {return q.empty() == false;});
-		// 1.락을 잡고
-		// 2. 조건 확인 해주고
-		// 3. 조건 맞으면 진행, 아니면 다시 대기 모드
-
-		int32 data = q.front();
-		q.pop();
-		cout << q.size() << endl;
-
-	
-	}
-};
-
+	int64 sum = 0;
+	for (int32 i = 0; i < 1'00000'0000; i++)
+		sum += i;
+	return sum;
+}
 int main()
 {
+	std::future<int64> future = std::async(std::launch::async, Calculate);
+	//2가지 옵션이 있음
+	//deffered -> 싱글스레드로 단순 함수의 동작을 나중으로 미뤄서 하는것
+	//async -> 스레드를 하나 생성해서 함수를 동작시킴
+	cout << "ㅎㅇ" << endl;
+	int64 result = future.get();
+	cout << "ㅎㅇ2" << endl;
+	cout << result << endl;
 
-	thread t1(Producer);
-	thread t2(Consumer);
-
-	t1.join();
-	t2.join();
 }
