fbd9098a5f9047bd1e6badb4bbfff307e1b48454
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2025-07-17 17:51:48 +0900

    Lock Queue, Lock Stack

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,29 +4,39 @@
 #include <atomic>
 #include <mutex>
 #include<future>
+#include<Windows.h>
+#include "ConcurrentQueue.h";
+#include "ConcurrentStack.h";
 
+LockStack<int32> s;
+LockQueue<int32> q;
 
-
-thread_local int32 LTreadID = 0;
-
-void ThreadMain(int32 _threadID)
+void Push()
 {
 	while (true)
 	{
-		LTreadID = _threadID;
-		cout << "Hello Thread!" << LTreadID << endl;
-		this_thread::sleep_for(1s);
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+		if (q.TryPop(OUT data))
+		{
+			cout << data << endl;
+		}
 	}
 }
 int main()
 {
-	vector<thread> threads;
-	for (int32 i = 0; i < 10; i++)
-	{
-		int32 threadID = i + 1;
-		
-		threads.push_back(thread(ThreadMain, threadID));
-	}
-	for (thread& t : threads)
-		t.join();
+	thread t1(Push);
+	thread t2(Pop);
+
+	t1.join();
+	t2.join();
 }
