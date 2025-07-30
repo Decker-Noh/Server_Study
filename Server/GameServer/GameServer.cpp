eb8bb7207f7f87c2370f9dacddfaa1d54804775f
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2025-07-30 20:45:28 +0900

    ref Count, Shared Ptr

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -6,30 +6,52 @@
 #include<future>
 #include<Windows.h>
 #include<ThreadManager.h>
+#include <vector>
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
+atomic<int32> sum;
 
-
+void Calculator(int val)
+{
+	if (val == 1)
+		return;
+	if (val == 2 || val == 3)
+	{
+		sum++;
+		return;
+	}
+	for (int i = 2; i < val; i++)
+	{
+		if ((val % i) == 0)
+			return;
+	}
+	sum++;
+}
+void Machin(int start, int end)
+{
+	for (int i = start; i <= end; i++)
+	{
+		Calculator(i);
+	}
+}
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "Player Then Account" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "Account Then Player" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
-	GThreadManager->Join();
+	const int MAX_NUMBER = 100'0000;
+
+	vector<thread> threads;
+	int coreNum = std::thread::hardware_concurrency();
+	
+	int coreWork = (MAX_NUMBER / coreNum) + 1;
+
+	for (int i = 0; i < coreNum; i++)
+	{
+		threads.push_back(thread(Machin, (i * coreWork) + 1, min(MAX_NUMBER,((i + 1) * coreWork))));
+
+	}
+	for (int i = 0; i < coreNum; i++)
+	{
+		threads[i].join();
+
+	}
+	cout << sum << endl;
+
 }
